84f21861b5612cc23bfc66d3e1e0a0f5b83fdb83
Najmunda <najmundaakmal@gmail.com>
2025-04-21 22:01:32 +0700

    fix: Fix link replace, add endpoint after db name

--- routes/cardDb.js
+++ routes/cardDb.js
@@ -4,11 +4,12 @@ const PouchDB = require("../db/cards.js");
 try {
   const router = new PromiseRouter();
 
-  const db = require("express-pouchdb")(PouchDB.config);
+  const db = require("express-pouchdb")(PouchDB.config, {mode: 'minimumForPouchDB'});
 
   router.use("/", function (req, res) {
     if (req.isAuthenticated()) {
       try {
+        req.url = `/db-${req.user.username}${req.url}`;
         db(req, res);
       } catch (error) {
         return res.status(500).send('Error occured on PouchDB.')
