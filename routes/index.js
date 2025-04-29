a9af288654afc3417a4934040754ee3bba05b3f2
Najmunda <najmundaakmal@gmail.com>
2025-04-29 23:34:13 +0700

    feat: Add server status endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const user = require("./user.js");
 const cardDb = require("./cardDb.js");
 const authGoogle = require("./authGoogle.js");
+const server = require("./server.js");
 
 function mountRoutes(app) {
   app.use('/user', user);
   app.use("/db", cardDb);
   app.use("/user", authGoogle);
+  app.use("/server", server);
 }
 
 module.exports = mountRoutes;
