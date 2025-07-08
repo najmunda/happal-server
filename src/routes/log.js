48a62b1b6e85de6265ecf72e93da35a996e38ca6
Najmunda <najmundaakmal@gmail.com>
2025-07-08 11:36:17 +0700

    feat(route): validation, error handle & response on /log/client

--- src/routes/log.js
+++ src/routes/log.js
@@ -7,15 +7,20 @@ const router = new PromiseRouter()
 // General Users/Auth Routes
 router.post('/client', async function (req, res, next) {
   const { log } = req.body
-  if (log !== '') {
+  if (Array.isArray(log)) {
     await db.query(
       format(
         'INSERT INTO client_logs ("timestamp", "level", "type", "service", "message", stack, user_agent) SELECT * FROM json_populate_recordset(NULL::client_log, %L)',
         JSON.stringify(log)
       )
-    )
+    ).catch(error => {
+      const handledError = new Error('', { cause: error });
+      throw handledError;
+    });
+  } else {
+    return res.status(400).send('Invalid logs format')
   }
-  return res.status(200).send('Client log sent.')
+  return res.status(200).send('Client logs sent')
 })
 
 module.exports = router
