45222e565532c3bfb01445b65da7b6cbe3dddce3
Najmunda <najmundaakmal@gmail.com>
2025-07-08 14:53:49 +0700

    feat(log): add client log validation & change response format
    response format to jsend & route dont process empty array

--- src/routes/log.js
+++ src/routes/log.js
@@ -7,20 +7,24 @@ const router = new PromiseRouter()
 // General Users/Auth Routes
 router.post('/client', async function (req, res, next) {
   const { log } = req.body
-  if (Array.isArray(log)) {
-    await db.query(
-      format(
-        'INSERT INTO client_logs ("timestamp", "level", "type", "service", "message", stack, user_agent) SELECT * FROM json_populate_recordset(NULL::client_log, %L)',
-        JSON.stringify(log)
+  if (Array.isArray(log) && log.length !== 0) {
+    await db
+      .query(
+        format(
+          'INSERT INTO client_logs ("timestamp", "level", "type", "service", "message", stack, user_agent) SELECT * FROM json_populate_recordset(NULL::client_log, %L)',
+          JSON.stringify(log)
+        )
       )
-    ).catch(error => {
-      const handledError = new Error('', { cause: error });
-      throw handledError;
-    });
+      .catch((error) => {
+        const handledError = new Error('', { cause: error })
+        throw handledError
+      })
   } else {
-    return res.status(400).send('Invalid logs format')
+    return res
+      .status(400)
+      .json({ status: 'fail', data: { logs: 'Invalid logs format' } })
   }
-  return res.status(200).send('Client logs sent')
+  return res.status(200).json({ status: 'success', data: null })
 })
 
 module.exports = router
