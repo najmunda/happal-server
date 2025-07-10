7a6fe3a26bd32b024ece2f0d16c04fb2e474e880
Najmunda <najmundaakmal@gmail.com>
2025-07-10 13:54:06 +0700

    feat(route): Change /server/status response to jsend

--- src/routes/server.js
+++ src/routes/server.js
@@ -3,7 +3,7 @@ const PromiseRouter = require('express-promise-router')
 const router = new PromiseRouter()
 
 router.get('/status', function (req, res) {
-  return res.status(200).send('Online')
+  return res.status(200).json({ status: 'success', data: { online: true } })
 })
 
 module.exports = router
